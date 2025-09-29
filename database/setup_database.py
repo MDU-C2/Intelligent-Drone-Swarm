3f42eeb97b1cbd2d1a0d39410601d940cfe5177f
seclnam <cnu21001@student.mdu.se>
2025-09-29 18:40:53 +0200

    Change so that user enters the name of db they want (#12)

--- database/setup_database.py
+++ database/setup_database.py
@@ -7,7 +7,19 @@ from connect_database import connect_database
 from create_tables import create_tables
 
 if __name__ == "__main__":
-    with connect_database("example.db") as db:
-        tables = create_tables(db.cursor)
-        tables.create_all_tables()
-        print("Database and all tables created successfully!")
+
+    print("Please enter the name of the databse you want to open(with .db):\n") 
+    db_name = input().strip()
+    try:
+        if not db_name.endswith(".db"):
+            raise ValueError("Database name must end with .db")
+        with open("db_name.txt", "w") as f:
+            f.write(db_name)
+        print(f"Database name '{db_name}' successfully written to db_name.txt.")
+        with connect_database(db_name) as db:
+            tables = create_tables(db.cursor)
+            tables.create_all_tables()
+            print("Database and all tables created successfully!")
+    except Exception as e:
+        print(f"Error writing to db_name.txt: {e}, please try again.")
+        exit(1)
