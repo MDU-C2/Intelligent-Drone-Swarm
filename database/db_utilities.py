ad19cc61f3c1c58d7f212cdeac96f24de511e8b8
Sir-Camp-A-Lot <ahd20002@student.mdu.se>
2025-10-13 19:07:34 +0200

    Update db_utilities.py

--- database/db_utilities.py
+++ database/db_utilities.py
@@ -7,6 +7,7 @@ class db_utilities:
     def delete_from_table(self, table_name, condition_column, condition_value):
         sql = f"DELETE FROM {table_name} WHERE {condition_column} = ?"
         self.cursor.execute(sql, (condition_value,))
+        return self.cursor.rowcount
 
     def check_requirement_exists(self, check_table, check_column, check_value):
         sql = f"SELECT 1 FROM {check_table} WHERE {check_column} = ? LIMIT 1"
@@ -16,3 +17,4 @@ class db_utilities:
     def update_row(self, table_name, set_column, set_value, condition_column, condition_value):
         sql = f"UPDATE {table_name} SET {set_column} = ? WHERE {condition_column} = ?"
         self.cursor.execute(sql, (set_value, condition_value))
+        return self.cursor.rowcount
